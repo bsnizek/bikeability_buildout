37efd4e53c9ad09a65bb8ee93e8099c3a877888a
Bernhard Snizek <bs@metascapes.org>
2011-05-14 00:50:57 +0200

    OSX App builder added

---
+++ makeapplication.py
@@ -0,0 +1,21 @@
+### makeapplication.py
+from bundlebuilder import buildapp
+  
+buildapp(
+         name='MapMmtcher.app', # what to build
+         mainprogram='sl/mapmatching/Mapmatcher.py', # your app's main()
+         argv_emulation=1, # drag&dropped filenames show up in sys.argv
+         iconfile='myapp.icns', # file containing your app's icons
+         standalone=1, # make this app self contained.
+         includeModules=[], # list of additional Modules to force in
+         includePackages=[
+                          'PyYAML',
+                          'Shapely',
+                          'RTree',
+                          ], # list of additional Packages to force in
+         libs=[
+               'spatialindex',
+               'sip',
+               ], # list of shared libs or Frameworks to include
+         )
+### end of makeapplication.py
